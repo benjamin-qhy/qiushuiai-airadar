591e0cc2864650223c62a62df4e8fd26aa21024f
秋水 <qiushui@qiushuideMac-mini.local>
2026-09-14 21:09:48 +0800

    test: allow Windows filesystem latency

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { configDefaults, defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     exclude: [...configDefaults.exclude, '**/dist/**'],
+    testTimeout: 15_000,
   },
   resolve: {
     alias: {
